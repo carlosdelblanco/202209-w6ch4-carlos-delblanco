daebc85ebbd6c2611442d62c75099fab49b0944d
Carlos del Blanco <delblanco.carlos@gmail.com>
2022-11-03 21:22:03 +0100

    Add types.ts

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import express from "express";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+const { log } = console;
 const port = process.env.PORT;
 const app = express();
 
 const server = app.listen(port, () => {
-  console.log(`Listening on http://localhost port ${port}`);
+  log(`Listening on http://localhost port ${port}`);
 });
 
 app.use((req, res) => {
